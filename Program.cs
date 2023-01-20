c640541eaa8b1ecaa341e13d9aaf0d3e2ee78910
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-01-20 22:55:18 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int sayi = 10;
+float sayi2 = 5.3f;
+double d = 20.6;
+decimal dec = 10.23m;
+
+System.Console.WriteLine(sayi);
+System.Console.WriteLine(sayi2);
+System.Console.WriteLine(d);
+System.Console.WriteLine(dec);
+
+string name = "sadık turan";
+char c = 'c';
+
+bool isRetired = true;
+bool isActive = false;
+
+System.Console.WriteLine(name);
+System.Console.WriteLine(c);
+System.Console.WriteLine(isRetired);
+System.Console.WriteLine(isActive);
