aa28ad40820f8b56bcad59bfc1df80d79256857a
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-01-20 23:13:34 +0300

    bölüm 2 tamamlandı.

---
+++ bolum2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Kilo bilgisini tutacak bir değişken (byte, short, int, long)
+byte? kilo = 90;      // soru işareti(?) ile ilgili değişkene null değer atanabileceği sağlanmış olur.
+
+// Plaka bilgisini tutacak bir değişken
+byte plaka = 81;
+string _plaka = "81";
+
+// Araç km bilgisini tutan bir değişken
+int km = 100000;
+string _km = "100000";
+
+// Müşteri id'sini tutan bir değişken
+string musteri_id = "A10234211";
+
+// Bir ürünün satışta olup olmadığı bilgisini tutacak değişken
+bool satistami = true;
+bool _satistami = false;
+
+// Maaş bilgisini tutacak bir değişken(float, double, decimal)
+decimal maas = 3000.3123m;
+
+// Öğrenci adını ve soyadını tutacak bir değişken
+string ad = "Sadık";
+string soyad = "Turan";
+string fullname = ad + " " + soyad;
+string adsoyad = "Sadık Turan";
+
+// Şube kodunu tutacak bir değişken (Örneğin, A)
+char sube = 'A';
+string _sube = "10A";
